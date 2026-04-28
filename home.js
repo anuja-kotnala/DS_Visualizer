50dea1a850883a9080d2df94a2035a0d45473047
Anuja Kotnala <anujakotnala2209@gmail.com>
2026-04-28 22:14:03 +0530

    completed home page

---
+++ home.js
@@ -0,0 +1,14 @@
+const b1=document.getElementById("stack");
+b1.addEventListener("click",function(){
+    window.location.href="pages/stack.html"
+});
+
+const b2= document.getElementById("q");
+b2.addEventListener("click",function(){
+    window.location.href="pages/queue.html";
+});
+
+const b3=document.getElementById("ll");
+b3.addEventListener("click", function(){
+    window.location.href="pages/ll.html";
+});
